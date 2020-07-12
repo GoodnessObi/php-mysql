36d1e1004c165cc58c594c53b5c9cfd62e114322
goodness obi <goodnessobi@gmail.com>
2020-07-12 21:43:27 +0100

    strings

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
 <?php  
 
-	// $name = 'Tope';
-	$age = 30;
+	$stringOne = 'my emial is ';
+	$stringTwo = 'tope123@yahoo.co.uk';
 
-	// $name = 'Gudy'
+	// echo "$stringOne.$stringTwo";
 
-	define('NAME', 'Tope');
+	$name = 'Tope';
 
+	// echo 'Hey! my name is ' .$name;
 
+	// echo "Hey! my name is $name";
+	// echo "The ninja screamed \"whaaaaaa\"";
+	// echo 'Gudy screamed "whaaaaa"';
+
+	// echo $name[0];
+
+	// echo strlen($stringOne);
+	// echo strtoupper($name);
+	// echo strtolower($name);
+	echo str_replace('o', 'i', $name);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>My first PHP file</title>
+	<title>PHP tutorial</title>
 </head>
 <body>
-	<h1> User profile Page</h1>
-
-	<div><?php echo NAME; ?></div>
-	<div><?php echo $age; ?></div>
+	
 
 
 </body>
