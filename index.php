7392a06030800680d6a920ccf47d3ccadb7199e4
goodness obi <goodnessobi@gmail.com>
2020-07-13 01:22:55 +0100

    break and continue

--- index.php
+++ index.php
@@ -11,31 +11,20 @@ $products = [
 	['name' => 'milk', 'price' => 45]
 ];
 
-// $price = 20;
-
-// if ($price < 10) {
-// 	echo 'the condition is met';
-// } elseif ($price < 30) {
-// 	echo "condition met";
-// } else {
-// 	echo "condition not met";
-// }
 
 
-// foreach ($products as $product) {
-// 	if ($product['price'] < 60 && $product['price'] > 10) {
-// 		echo $product['name'];
-// 		echo "<br/>";
-// 	}
-// }
+foreach ($products as $product) {
+	if ($product['name'] === 'sugar') {
+		break;
+	}
+
+	if ($product['price'] > 60) {
+		continue;
+	}
+	echo $product['name'] . '<br/>';
+}
 
 
-// foreach ($products as $product) {
-// 	if ($product['price'] > 60 || $product['price'] < 20) {
-// 		echo $product['name'];
-// 		echo "<br/>";
-// 	}
-// }
 
 ?>
 
@@ -45,18 +34,7 @@ $products = [
 	<title>PHP tutorial</title>
 </head>
 <body>
-	
-	<div>
-		<ul>
-			<?php foreach($products as $product) { ?>
-				<?php if ($product['price'] >10){ ?>
-					<li><?php echo $product['name'] . '<br/>' ?></li>
-					
-				<?php } ?>
 
-			<?php } ?>
-		</ul>
-	</div>
 
 
 
