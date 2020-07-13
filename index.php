27ca321a6e614072b047de533b64681e3503efcd
goodness obi <goodnessobi@gmail.com>
2020-07-13 01:41:30 +0100

    functions

--- index.php
+++ index.php
@@ -1,29 +1,24 @@
 <?php  
 
-//conditional statements
+//function
 
-$products = [
-	['name' => 'cornflakes', 'price' => 100],
-	['name' => 'beverage', 'price' => 60],
-	['name' => 'bleach', 'price' => 80],
-	['name' => 'soap', 'price' => 10],
-	['name' => 'sugar', 'price' => 60],
-	['name' => 'milk', 'price' => 45]
-];
-
-
-
-foreach ($products as $product) {
-	if ($product['name'] === 'sugar') {
-		break;
-	}
-
-	if ($product['price'] > 60) {
-		continue;
-	}
-	echo $product['name'] . '<br/>';
+function sayHello($name = 'user', $time = 'morning') {
+	echo "good $time $name";
 }
 
+//sayHello('gudy');
+
+sayHello('tope', 'evening');
+
+function formatProduct($product) {
+	//echo "{$product['name']} cost {$product['price']} pounds to buy <br/>";
+	return "{$product['name']} cost {$product['price']} pounds to buy <br/>";
+}
+
+
+// $formatted = formatProduct(['name' => 'cornflakes', 'price' => 50]);
+
+// echo $formatted;
 
 
 ?>
