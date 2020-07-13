7803565e116ed5695df15ecef5f2d1f36ee75da9
goodness obi <goodnessobi@gmail.com>
2020-07-13 16:52:08 +0100

    feature: render data to browser

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 	//write query for all pizzas
 
-	$sql = 'SELECT title, ingredients, id FROM pizzas';
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
 	//make query and get results
 
@@ -26,7 +26,7 @@
 	mysqli_close($conn);
 
 
-	print_r($pizzas);
+	// print_r($pizzas);
 	
 ?>
 
@@ -34,12 +34,28 @@
 <html>
 
 	<?php include ('templates/header.php'); ?> 
+
+	<h4 class="center grey-text">Pizzas!</h4>
+	<div class="container">
+		<div class="row">
+			<?php foreach ($pizzas as $pizza) { ?>
+				
+				<div class="col s6 md3">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+						</div>
+						<div class="card-action right-align">
+							<a class="brand-text" href="#">more info</a>
+						</div>
+					</div>
+				</div>
+
+			<?php } ?>
+		</div>
+	</div>
 	
-
 	<?php include ('templates/footer.php'); ?> 
-
-
-
-
 
 </htmlxxs 
