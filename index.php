ef44350e84a6f5a509e46b97c4b471e448b6d8f5
goodness obi <goodnessobi@gmail.com>
2020-07-13 12:04:41 +0100

    feature: add header and footer

--- index.php
+++ index.php
@@ -1,37 +1,21 @@
 <?php  
 
-//function
-
-function sayHello($name = 'user', $time = 'morning') {
-	echo "good $time $name";
-}
-
-//sayHello('gudy');
-
-sayHello('tope', 'evening');
-
-function formatProduct($product) {
-	//echo "{$product['name']} cost {$product['price']} pounds to buy <br/>";
-	return "{$product['name']} cost {$product['price']} pounds to buy <br/>";
-}
 
 
-// $formatted = formatProduct(['name' => 'cornflakes', 'price' => 50]);
-
-// echo $formatted;
 
 
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>PHP tutorial</title>
-</head>
-<body>
+
+	<?php include ('templates/header.php'); ?> 
+	
+
+	<?php include ('templates/footer.php'); ?> 
 
 
 
 
 </body>
-</html> 
+</html>
---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<head>
+	<title>Gudy Pizza</title>
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+
+    <!-- Compiled and minified JavaScript -->
+  <!--   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> -->
+</head>
+<body class="grey lighten-4">
+		<nav class="white z-depth-0">
+			<div class="container">
+				<a href="#" class="brand-logo brand-text">Gudy Pizza</a>
+				<ul id="nav-mobile" class="right hide-on-small-and-down">
+					<li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+				</ul>
+			</div>
+		</nav>
