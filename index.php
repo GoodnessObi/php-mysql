7d9863e76318f7102b3d9cadb7d8e1bbc5f6fdb6
goodness obi <goodnessobi@gmail.com>
2020-07-13 01:16:47 +0100

    conditional statements

--- index.php
+++ index.php
@@ -1,30 +1,41 @@
 <?php  
 
-//comparisons booleans
+//conditional statements
 
-	// echo 5 < 10;
-	// echo 5 > 10;
-	// echo 5 == 10;
-	// echo 10 == 10;
-	// echo 5 != 10;
-	// echo 5 <= 5;
-	// echo 5 >= 5;
+$products = [
+	['name' => 'cornflakes', 'price' => 100],
+	['name' => 'beverage', 'price' => 60],
+	['name' => 'bleach', 'price' => 80],
+	['name' => 'soap', 'price' => 10],
+	['name' => 'sugar', 'price' => 60],
+	['name' => 'milk', 'price' => 45]
+];
 
-//echo 'shaun' < 'yoshi';
+// $price = 20;
 
-// echo 'shaun' < 'Shaun';
-//echo 'gudy' == 'Gudy';
-
-
-//loose vs strict equal comparison
-
-//echo 5 == '5';
-//echo 5 === '5';
-
-echo true == "1";
-echo false == "";
+// if ($price < 10) {
+// 	echo 'the condition is met';
+// } elseif ($price < 30) {
+// 	echo "condition met";
+// } else {
+// 	echo "condition not met";
+// }
 
 
+// foreach ($products as $product) {
+// 	if ($product['price'] < 60 && $product['price'] > 10) {
+// 		echo $product['name'];
+// 		echo "<br/>";
+// 	}
+// }
+
+
+// foreach ($products as $product) {
+// 	if ($product['price'] > 60 || $product['price'] < 20) {
+// 		echo $product['name'];
+// 		echo "<br/>";
+// 	}
+// }
 
 ?>
 
@@ -35,6 +46,17 @@ echo false == "";
 </head>
 <body>
 	
+	<div>
+		<ul>
+			<?php foreach($products as $product) { ?>
+				<?php if ($product['price'] >10){ ?>
+					<li><?php echo $product['name'] . '<br/>' ?></li>
+					
+				<?php } ?>
+
+			<?php } ?>
+		</ul>
+	</div>
 
 
 
