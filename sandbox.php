6d04bddef687c78bcf2b6997cc718fb1747193c5
goodness obi <goodnessobi@gmail.com>
2020-07-21 09:59:32 +0100

    objects and classes

--- sandbox.php
+++ sandbox.php
@@ -4,8 +4,8 @@
 
 	class User {
 
-		public $email;
-		public $name;
+		private $email;
+		private $name;
 
 		public function __construct($name, $email){
 			// $this->email = 'tope@yahoo.com';
@@ -19,17 +19,28 @@
 			echo $this->name . " logged in.";
 		}
 
+		public function getName(){
+			return $this->name;
+		}
+
+		public function setName($name){
+			if(is_string($name) && strlen($name) > 1) {
+				$this->name =$name;
+				return "name has been updated to $name";
+			} else {
+				return 'not a valid name';
+			}
+		}
+
 	}
-
-	// $userOne = new User();
-
-	// $userOne->login();
-	// echo $userOne->name;
  
 	$userTwo = new User('rere', 'wiwa@woo.com');
-	echo $userTwo->name;
-	// echo $userTwo->email;
-	// $userTwo->login();
+	 //echo $userTwo->getName();
+	 //echo $userTwo->setName(50);
+	$userTwo->setName('Shaun');
+	echo $userTwo->getName();
+	// echo $userTwo->email
+		// $userTwo->login();
 
 ?>
 
