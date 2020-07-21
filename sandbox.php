2e836975e0f0716f843a33b70537a12d381279ae
goodness obi <goodnessobi@gmail.com>
2020-07-21 06:56:24 +0100

    ternary operators

---
+++ sandbox.php
@@ -0,0 +1,28 @@
+<?php 
+
+//ternary operators
+
+$score = 50;
+
+// if ($score > 40){
+// 	echo "high score!";
+// } else {
+// 	echo "low score :(";
+// }
+
+//echo $score > 40 ? 'high score!' : 'low score :(';
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php tut</title>
+</head>
+<body>
+
+	<p><?php echo $score > 40 ? 'high score!' : 'low score :('; ?></p>	
+
+</body>
+</html>
